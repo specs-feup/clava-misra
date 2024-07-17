5b61335f1509743cab50a043273b8ab586c10f66
Afonso Osorio <up202108700@edu.fe.up.pt>
2024-07-17 19:42:20 +0100

    skeleton of new class

---
+++ src/misra/MISRAReporter.ts
@@ -0,0 +1,39 @@
+import { Decl, FunctionJp } from "clava-js/api/Joinpoints.js";
+import MISRAAnalyser from "./MISRAAnalyser.js";
+
+export interface MISRAPreprocessing {
+    externalObjects?: Array<FunctionJp | Decl>;
+}
+
+export enum MISRAPreprocessingReqs {
+    EXTERNAL_OBJECTS
+}
+
+export default class MISRAReporter {
+    #analysers: MISRAAnalyser[];
+    #preprocessing: MISRAPreprocessing = {};
+
+    constructor(analysers: MISRAAnalyser[]) {
+        this.#analysers = analysers;
+    }
+
+    private updateReqs(reqs: MISRAPreprocessingReqs[]) {
+        for (const req of reqs) {
+            switch (req) {
+                case MISRAPreprocessingReqs.EXTERNAL_OBJECTS:
+                    if (!this.#preprocessing.externalObjects) {
+                        //call to get all declarations of external objects
+                    } 
+                    break;
+            }
+        }
+    }
+
+    generateReport() {
+        for (const analyser of this.#analysers) {
+            const reqs = [MISRAPreprocessingReqs.EXTERNAL_OBJECTS];
+            this.updateReqs(reqs);
+            analyser.analyse(this.#preprocessing);
+        }
+    }
+}
