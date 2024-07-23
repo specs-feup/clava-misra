375eb019ef5526201ae8a5d1e8ea4044511d9e73
Afonso Osorio <up202108700@edu.fe.up.pt>
2024-07-23 14:27:08 +0100

    migrate section 21

---
+++ src/misra/passes/S21_StandardLibPass.ts
@@ -0,0 +1,81 @@
+import { LaraJoinPoint } from "lara-js/api/LaraJoinPoint.js";
+import MISRAPass from "../MISRAPass.js";
+import { PreprocessingReqs } from "../MISRAReporter.js";
+import { Call, Include, Joinpoint } from "clava-js/api/Joinpoints.js";
+
+export default class S21_StandardLibPass extends MISRAPass {
+    protected _preprocessingReqs: PreprocessingReqs[] = [];
+
+    initRuleMapper(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    matchJoinpoint($jp: LaraJoinPoint): boolean {
+        return $jp instanceof Call || $jp instanceof Include;
+    }
+
+    private r21_3_noDynamicAllocation($startNode: Joinpoint) {
+        if (!($startNode instanceof Call && /(malloc|realloc|calloc|free)/.test($startNode.name))) return;
+
+        this.logMISRAError("Use of memory allocation functions provided by <stdlib.h> is not allowed.");
+    }
+
+    private r21_4_noSetjmpUsage($startNode: Joinpoint) {
+        if (!($startNode instanceof Include && $startNode.isAngled && $startNode.name === "setjmp.h")) return;
+
+        this.logMISRAError("Use of <setjmp.h> is not allowed.");
+    }
+    
+    private r21_5_noSignalUsage($startNode: Joinpoint) {
+        if (!($startNode instanceof Include && $startNode.isAngled && $startNode.name === "signal.h")) return;
+
+        this.logMISRAError("Use of <signal.h> is not allowed.");    
+    }
+
+    private r21_6_noStandardIO($startNode: Joinpoint) {
+        if (!($startNode instanceof Include && $startNode.isAngled && /(stdio.h|wchar.h)/.test($startNode.name))) return;
+
+        this.logMISRAError("Use of the standard library I/O functions provided by <stdio.h> and <wchar.h> is not allowed.");
+    }
+
+    private r21_7_noStringFunctions($startNode: Joinpoint) {
+        if (!($startNode instanceof Call && /(atoi|atof|atol|atoll)/.test($startNode.name))) return;
+
+        this.logMISRAError(`Use of function ${$startNode.signature} is not allowed.`);
+    }
+
+    private r21_8_noSysFunctions($startNode: Joinpoint) {
+        if (!($startNode instanceof Call && /(system|abort|exit|getenv)/.test($startNode.name))) return;
+        
+        this.logMISRAError(`Use of function ${$startNode.signature} is not allowed.`);
+    }
+
+    private r21_9_noStdAlgos($startNode: Joinpoint) {
+        if (!($startNode instanceof Call && /(qsort|bsearch)/.test($startNode.name))) return;
+        
+        this.logMISRAError(`Use of function ${$startNode.signature} is not allowed.`);
+    }
+
+    private r21_10_noDateUsage($startNode: Joinpoint) {
+        if ($startNode instanceof Include && $startNode.isAngled && $startNode.name === "time.h") {
+            this.logMISRAError("Use of <time.h> is not allowed.");
+        }
+        else if ($startNode instanceof Call && $startNode.name === "wcsftime") {
+            this.logMISRAError("Identifier 'wcsftime' shall not be used.");
+        }
+    }
+    
+    private r21_11_noTgmathUsage($startNode: Joinpoint) {
+        if (!($startNode instanceof Include && $startNode.isAngled && $startNode.name === "tgmath.h")) return;
+
+        this.logMISRAError("Use of <tgmath.h> is not allowed."); 
+    }
+
+    private r21_12_noFenvExceptions($startNode: Joinpoint) {
+        if (!($startNode instanceof Call && /(feclearexcept|fegetexceptflag|feraiseexcept|fesetexceptflag|fetestexcept)/.test($startNode.name))) return;
+        
+        this.logMISRAError(`Use of function ${$startNode.signature} is not allowed.`);
+    }
+
+    protected _name: string;
+}
