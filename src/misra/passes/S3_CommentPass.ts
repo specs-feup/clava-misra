de89f79d30711789d9e561b38ba769a64cf69b44
Afonso Osorio <up202108700@edu.fe.up.pt>
2024-07-24 17:28:47 +0100

    better logging in comments

--- src/misra/passes/S3_CommentPass.ts
+++ src/misra/passes/S3_CommentPass.ts
@@ -2,6 +2,7 @@ import { LaraJoinPoint } from "lara-js/api/LaraJoinPoint.js";
 import MISRAPass from "../MISRAPass.js";
 import { PreprocessingReqs } from "../MISRAReporter.js";
 import { Comment, Joinpoint } from "clava-js/api/Joinpoints.js";
+import Fix from "clava-js/api/clava/analysis/Fix.js";
 
 export default class S3_CommentPass extends MISRAPass {
     protected _preprocessingReqs: PreprocessingReqs[] = [];
@@ -21,10 +22,18 @@ export default class S3_CommentPass extends MISRAPass {
     }
     
     private r3_1_fixComments($startNode: Joinpoint) { //inlines
-        if ($startNode instanceof Comment) {
-            $startNode.setText(S3_CommentPass.removeCommentSequences($startNode.text));
+        if ($startNode instanceof Comment && /(\/\/|\/\*)/g.test($startNode.text)) {
+            this.logMISRAError(`Comment ${$startNode.text} contains invalid character sequences.`, new Fix(
+                $startNode,
+                $jp => ($jp as Comment).setText(S3_CommentPass.removeCommentSequences($startNode.text))
+            ));
         } 
-        $startNode.inlineComments.forEach(comment => comment.setText(S3_CommentPass.removeCommentSequences(comment.text)), this);
+        $startNode.inlineComments.filter(comment => /(\/\/|\/\*)/g.test(comment.text)).forEach(comment => {
+            this.logMISRAError(`Comment ${comment.text} contains invalid character sequences.`, new Fix(
+                $startNode,
+                $jp => ($jp as Comment).setText(S3_CommentPass.removeCommentSequences(comment.text))
+            ));
+        });
     }
 
     protected _name: string = "Comments";
