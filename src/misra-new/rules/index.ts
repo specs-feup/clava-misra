6870e111dd4b8fc6a90f68ca0049a56b77c95467
Milena Gouveia <milenaagouveia@gmail.com>
2025-02-26 12:01:29 +0000

    Remove import for Rule_2_6_UnusedLabels

--- src/misra-new/rules/index.ts
+++ src/misra-new/rules/index.ts
@@ -1,5 +1,4 @@
 import MISRAContext from "../MISRAContext.js";
-import Rule_2_6_UnusedLabels from "./Section2_UnusedCode/Rule_2_6_UnusedLabels.js";
 import Rule_3_1_CommentSequences from "./Section3_Comments/Rule_3_1_CommentSequences.js";
 import Rule_3_2_CommentSequences from "./Section3_Comments/Rule_3_2_LineSplicing.js";
 
