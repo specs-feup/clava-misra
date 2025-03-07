696fc9a04e0538c209986ba97cee4da4ff161c00
Milena Gouveia <milenaagouveia@gmail.com>
2025-03-07 23:04:22 +0000

    Implement Rule 2.4 - unused tag declarations

--- src/misra-new/rules/index.ts
+++ src/misra-new/rules/index.ts
@@ -1,5 +1,6 @@
 import MISRAContext from "../MISRAContext.js";
 import Rule_2_3_UnusedTypeDecl from "./Section2_UnusedCode/Rule_2_3_UnusedTypeDecl.js";
+import Rule_2_4_UnusedTagDecl from "./Section2_UnusedCode/Rule_2_4_UnusedTagDecl.js";
 import Rule_2_6_UnusedLabels from "./Section2_UnusedCode/Rule_2_6_UnusedLabels.js";
 import Rule_2_7_UnusedParameters from "./Section2_UnusedCode/Rule_2_7_UnusedParameters.js";
 import Rule_3_1_CommentSequences from "./Section3_Comments/Rule_3_1_CommentSequences.js";
@@ -8,6 +9,7 @@ import Rule_3_2_CommentSequences from "./Section3_Comments/Rule_3_2_LineSplicing
 export function misraRules(context: MISRAContext) {
     return [
         new Rule_2_3_UnusedTypeDecl(context),
+        new Rule_2_4_UnusedTagDecl(context),
         new Rule_2_6_UnusedLabels(context),
         new Rule_2_7_UnusedParameters(context),
         new Rule_3_1_CommentSequences(context),
