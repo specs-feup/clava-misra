801022a87f691bcf61321b32a7ae558c32ebc4bc
Milena Gouveia <milenaagouveia@gmail.com>
2025-03-16 22:40:35 +0000

    Documentation for SwitchHasDeafult and DefaultFirstOrLast

--- src/misra-new/rules/Section16_SwitchStatements/Rule_16_4_SwitchHasDefault.ts
+++ src/misra-new/rules/Section16_SwitchStatements/Rule_16_4_SwitchHasDefault.ts
@@ -13,6 +13,12 @@ export default class Rule_16_4_SwitchHasDefault extends MISRARule {
         super("16.4", context);
     }
 
+    /**
+     * Checks if the given joinpoint is a switch statement with a default case
+     * @param $jp - Joinpoint to analyze
+     * @param logErrors - [logErrors=false] - Whether to log errors if a violation is detected
+     * @returns Returns true if the joinpoint violates the rule, false otherwise
+     */
     match($jp: Joinpoint, logErrors: boolean = false): boolean {
         if (!($jp instanceof Switch)) return false;
 
--- src/misra-new/rules/Section16_SwitchStatements/Rule_16_5_DefaultFirstOrLast.ts
+++ src/misra-new/rules/Section16_SwitchStatements/Rule_16_5_DefaultFirstOrLast.ts
@@ -5,7 +5,7 @@ import { MISRATransformationReport, MISRATransformationType } from "../../MISRA.
 import { isCommentStmt } from "../../utils.js";
 
 /**
- * MISRA Rule 16.5: A default label shall appear as either the fi rst or the last switch label of
+ * MISRA Rule 16.5: A default label shall appear as either the first or the last switch label of
 a switch statement
  */
 export default class Rule_16_5_DefaultFirstOrLast extends MISRARule {
@@ -14,6 +14,13 @@ export default class Rule_16_5_DefaultFirstOrLast extends MISRARule {
         super("16.5", context);
     }
 
+    /**
+     * Retrieves all consecutive case statements to the left of a given case statement
+     * 
+     * @param $jp - The starting case statement from which the search will begin
+     * @returns  Array containing consecutive case statements to the left of the provided joinpoint
+     * 
+     */
     private getConsecutiveRightCases($jp: Case): Joinpoint[] {
         const cases = [];
         for (const stmt of $jp.siblingsRight) {
@@ -24,6 +31,13 @@ export default class Rule_16_5_DefaultFirstOrLast extends MISRARule {
         return cases;
     }
 
+    /**
+     * Retrieves all consecutive case statements to the right of a given case statement
+     * 
+     * @param $jp - The starting case statement from which the search will begin
+     * @returns  Array containing consecutive case statements to the right of the provided joinpoint
+     * 
+     */
     private getConsecutiveLeftCases($jp: Case): Joinpoint[] {
         let cases: Joinpoint[] = [];
         for (let i = $jp.siblingsLeft.length - 1; i >= 0; i--) {
@@ -36,6 +50,12 @@ export default class Rule_16_5_DefaultFirstOrLast extends MISRARule {
         return cases.reverse();
     }
 
+    /**
+     * Checks if the given joinpoint is a switch statement where the default label appears either as first of last label
+     * @param $jp - Joinpoint to analyze
+     * @param logErrors - [logErrors=false] - Whether to log errors if a violation is detected
+     * @returns Returns true if the joinpoint violates the rule, false otherwise
+     */
     match($jp: Joinpoint, logErrors: boolean = false): boolean {
         if (!($jp instanceof Switch)) return false;
 
@@ -55,12 +75,20 @@ export default class Rule_16_5_DefaultFirstOrLast extends MISRARule {
         return false;
     }
 
+    /**
+     * Transforms a switch statement so that the default case appears as the last label
+     * - If the default case is not already the last case within its case clause list, it is repositioned
+     * - Then, the entire case clause list containing the default case to the end of the switch statement
+     * @param $jp - Joinpoint to transform
+     * @returns Report detailing the transformation result
+     */
     transform($jp: Joinpoint): MISRATransformationReport {
         if (!this.match($jp)) return new MISRATransformationReport(MISRATransformationType.NoChange);
 
         const defaultCase = ($jp as Switch).getDefaultCase;
-        const rightStatements = defaultCase.siblingsRight.filter(sibling => sibling instanceof Statement);
+        const rightStatements = defaultCase.siblingsRight.filter(sibling => !isCommentStmt(sibling));
        
+        //  Reposition the default case to the last position within its case clause lis
         if (rightStatements[0] instanceof Case) { // At least one of the first statements is a Case
             const rightConsecutiveCases = this.getConsecutiveRightCases(defaultCase);
             const lastRightCase = rightConsecutiveCases[rightConsecutiveCases.length - 1];
@@ -72,6 +100,7 @@ export default class Rule_16_5_DefaultFirstOrLast extends MISRARule {
             }
         }
 
+        // Move the entire case clause list containing the default case to the end of the switch statement
         const switchScope = $jp.children[1];
         const stmts = [...this.getConsecutiveLeftCases(defaultCase), defaultCase, ...defaultCase.instructions];
 
