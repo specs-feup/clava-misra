6bf690b7d991485b2b56026fa7941385691f3c5a
Milena Gouveia <milenaagouveia@gmail.com>
2025-04-10 18:31:30 +0100

    Fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import VisualizationTool from "@specs-feup/clava-visualization/api/VisualizationTool.js";
-import MISRATool from "./misra-new/MISRATool.js";
+import MISRATool from "./MISRATool.js";
 
 MISRATool.checkCompliance();
 MISRATool.applyCorrections();
