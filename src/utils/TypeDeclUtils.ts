dc57024149ba541bdb88f177daa57a7780a7a6ee
Milena Gouveia <milenaagouveia@gmail.com>
2025-08-18 16:47:30 +0100

    Fix rule 2.3

--- src/utils/TypeDeclUtils.ts
+++ src/utils/TypeDeclUtils.ts
@@ -40,9 +40,8 @@ export function jpUsesTypedef(jp: Joinpoint, typeDecl: TypedefDecl): boolean {
     const jpType = getBaseType(jp);
 
     return !jpType?.isBuiltin && 
-        jpType instanceof ElaboratedType && 
-        jpType.namedType instanceof TypedefType && 
-        jpType.namedType.decl.astId === typeDecl.astId;
+        jpType instanceof TypedefType && 
+        jpType.decl.astId === typeDecl.astId;
 }
 
 /**
